adb97affc98f5cf1245bf4c80bb4a8c6028fafc7
Avihai Navon <avihygerec@gmail.com>
2023-01-15 23:12:44 +0200

    09.01.23 13:44

--- src/RealEstate.java
+++ src/RealEstate.java
@@ -242,7 +242,7 @@ public class RealEstate {
                     case "3" -> printAllProperties(user);
                     case "4" -> printProperties(user);
                     case "5" -> search(user);
-                   // case "6" -> //mainMenu();
+                     case "6" -> System.out.println("Exit to the main menu");
                 }
             }
         } while (!newChoice.equals("6"));
