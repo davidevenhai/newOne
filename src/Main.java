bb4fe930131d3710c1aef07274409e199a4c38b1
David Even Haim <davidevenhai@gmail.com>
2023-01-15 17:44:36 +0200

    להגשה

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         RealEstate check = new RealEstate();
+//        System.out.println("David Even Haim, ID: 208060988");
+//        System.out.println("Avihay Ben Ami, ID: 308204742");
+//        System.out.println("Avihay Navon, ID: 206227654");
+//        System.out.println("Omer Hayon, ID:208108233 ");
 
     }
 }
